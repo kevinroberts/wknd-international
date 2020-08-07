8bfae4ba4a4a43b319fd41f7a1d0e6bc09469833
Kevin Roberts <kevin.roberts10@gmail.com>
2020-08-07 12:09:49 -0500

    bug fixes for simpsons quote

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/SimpsonsQuoteImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/SimpsonsQuoteImpl.java
@@ -53,32 +53,33 @@ public class SimpsonsQuoteImpl implements SimpsonsQuote {
                 quoteList = simpsonsQuoteService.getSimpsonsQuotes(1);
             }
 
-            for (int i = 0; i < quoteList.size(); i++) {
-                Quote quote = quoteList.get(i);
-                quote.setLikes(RandomUtils.nextInt(10, 500));
-                // reduce font size for longer quotes
-                if (quote.getQuote().length() > 177) {
-                    quote.setQuoteFontSize("22px");
-                } else if (quote.getQuote().length() > 94 && quote.getQuote().length() <= 177) {
-                    quote.setQuoteFontSize("24px");
-                } else if (quote.getQuote().length() >= 74 && quote.getQuote().length() <= 94) {
-                    quote.setQuoteFontSize("32px");
-                } else {
-                    quote.setQuoteFontSize("48px");
-                }
-                // set prev. and next quote indices
-                if (i == 0) {
-                    quoteList.get(0).setPrevQuote(numberOfQuotes);
-                    quoteList.get(0).setNextQuote(i + 2);
-                } else if (i == (quoteList.size() - 1)) {
-                    quoteList.get(i).setPrevQuote(i);
-                    quoteList.get(i).setNextQuote(1);
-                } else {
-                    quoteList.get(i).setPrevQuote(i);
-                    quoteList.get(i).setNextQuote(i + 2);
+            if (Objects.nonNull(quoteList) && quoteList.size() > 0) {
+                for (int i = 0; i < quoteList.size(); i++) {
+                    Quote quote = quoteList.get(i);
+                    quote.setLikes(RandomUtils.nextInt(10, 500));
+                    // reduce font size for longer quotes
+                    if (quote.getQuote().length() > 177) {
+                        quote.setQuoteFontSize("22px");
+                    } else if (quote.getQuote().length() > 94 && quote.getQuote().length() <= 177) {
+                        quote.setQuoteFontSize("24px");
+                    } else if (quote.getQuote().length() >= 74 && quote.getQuote().length() <= 94) {
+                        quote.setQuoteFontSize("32px");
+                    } else {
+                        quote.setQuoteFontSize("48px");
+                    }
+                    // set prev. and next quote indices
+                    if (i == 0) {
+                        quoteList.get(0).setPrevQuote(quoteList.size());
+                        quoteList.get(0).setNextQuote(i + 2);
+                    } else if (i == (quoteList.size() - 1)) {
+                        quoteList.get(i).setPrevQuote(i);
+                        quoteList.get(i).setNextQuote(1);
+                    } else {
+                        quoteList.get(i).setPrevQuote(i);
+                        quoteList.get(i).setNextQuote(i + 2);
+                    }
                 }
             }
-
         } catch (ApiException e) {
             LOG.error("API exception reached, ", e);
         }
@@ -91,6 +92,6 @@ public class SimpsonsQuoteImpl implements SimpsonsQuote {
 
     @Override
     public boolean isEmpty() {
-        return (Objects.isNull(quoteList));
+        return (Objects.isNull(numberOfQuotes));
     }
 }
